dad9135ceaa5d25d8b2916226108c9482d73bd83
Ikabl <ikbal.webx@gmail.com>
2022-07-01 21:09:38 +0600

    experiance update

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,26 +1,26 @@
-import React, { useContext } from 'react';
-import './index.scss'
-// import TypedText from "../TypedText";
+import { useContext } from "react";
+import "./index.scss";
 import SocialLinks from "../SocialLinks";
-// import Info from "../Info";
+import my_pic from "../../assets/ami.jpg";
+import { ThemeContext } from "../../context/ThemeContext";
+import { useAnimation } from "../../use/animation";
 import Skills from "../Skills";
-import {ThemeContext} from "../../context/ThemeContext";
-import {useAnimation} from "../../use/animation";
-import my_pic from '../../assets/ami.jpg';
-
 
 const Profile = () => {
-    const { backgroundImage, type } = useContext(ThemeContext);
-    const { animation } = useAnimation();
-    return (
-        <div className="profile">
-            <div className="profile__banner">
-                <div className={`profile__photo ${animation}`} style={{backgroundImage: `url(${backgroundImage})`}} />
-                <img src={my_pic} alt={type} />
-            </div>
-            <div className="profile__content">
-                <div className="profile__title">Ikbal Nayem</div>
-                {/* <TypedText
+	const { backgroundImage, type } = useContext(ThemeContext);
+	const { animation } = useAnimation();
+	return (
+		<div className="profile">
+			<div className="profile__banner">
+				<div
+					className={`profile__photo ${animation}`}
+					style={{ backgroundImage: `url(${backgroundImage})` }}
+				/>
+				<img src={my_pic} alt={type} />
+			</div>
+			<div className="profile__content">
+				<div className="profile__title">Ikbal Nayem</div>
+				{/* <TypedText
                   dataText={
                       [
                         'JavaScript Developer',
@@ -32,14 +32,11 @@ const Profile = () => {
                       ]
                   }
                 /> */}
-                <SocialLinks/>
-                {/**<Info icon="location">
-                                    Dhaka / Bangladesh
-                                </Info>**/}
-                <Skills />
-            </div>
-        </div>
-    );
+				<SocialLinks />
+				<Skills />
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
